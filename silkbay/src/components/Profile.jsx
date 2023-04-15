6c551016e6247228b107ac0cc2be258ddcdc16ff
NICO\pupi1 <nzambrana20@uma.es>
2023-04-15 10:38:06 +0200

    perfil css

--- silkbay/src/components/Profile.jsx
+++ silkbay/src/components/Profile.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import User_Icon from '../assets/icon_user.png'
 export default function(props){
     return(
         <>
-        <h1>Mi cuenta</h1>
-        <h2>Datos</h2>
-        <p>Nombre: {props.user}</p>
-
-        <h2>Historial de pedidos</h2>
+        <div className='user_data'>
+            <h1 className='my_account'>Mi cuenta</h1>
+            <div class="card" style="width: 50rem;">
+                <h2 className='data_title'>Datos</h2>
+                <img src={User_Icon} alt="User image" className='user-icon'/>
+                <p className='user_name'>Nombre: {props.user}</p>
+            </div>
+        </div>
+        <div className='user_orders'>
+            <h2 className='orders_title'>Historial de pedidos</h2>
+        </div>
         </>
     );
 }
